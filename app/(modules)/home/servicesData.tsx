c87c44af66644e7246b6e1c474e19bb5260b1c15
SODMIA <167714181+SODMIA@users.noreply.github.com>
2024-05-05 23:16:06 -0500

    feat: icons

--- app/(modules)/home/servicesData.tsx
+++ app/(modules)/home/servicesData.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 import pantone from "@/public/icons/pantone.png";
 import optimization from "@/public/icons/optimization.png";
 import consultingColor from "@/public/icons/consultingColor.png";
+import systemIntegration from "@/public/icons/systemIntegration.png";
+import encrypted from "@/public/icons/encrypted.png";
+import wrench from "@/public/icons/wrench.png";
 
 export const servicesData = [
   {
@@ -43,18 +46,12 @@ export const servicesData = [
     description:
       "Implementación de sistemas integrados para una gestión eficiente de la información.",
     icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="w-8 h-8 dark:text-violet-600"
-      >
-        <path
-          fillRule="evenodd"
-          d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z"
-          clipRule="evenodd"
-        ></path>
-      </svg>
+      <Image
+        width={40}
+        height={40}
+        src={systemIntegration}
+        alt="Desarrollo a medida"
+      />
     ),
   },
   {
@@ -62,18 +59,7 @@ export const servicesData = [
     description:
       "Implementación de medidas de seguridad avanzadas para proteger la integridad y confidencialidad de los datos del cliente.",
     icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="w-8 h-8 dark:text-violet-600"
-      >
-        <path
-          fillRule="evenodd"
-          d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z"
-          clipRule="evenodd"
-        ></path>
-      </svg>
+      <Image width={40} height={40} src={encrypted} alt="Desarrollo a medida" />
     ),
   },
   {
@@ -81,18 +67,7 @@ export const servicesData = [
     description:
       "Ofrecemos soporte continuo y mantenimiento para garantizar el funcionamiento óptimo del software desarrollado.",
     icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="w-8 h-8 dark:text-violet-600"
-      >
-        <path
-          fillRule="evenodd"
-          d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z"
-          clipRule="evenodd"
-        ></path>
-      </svg>
+      <Image width={40} height={40} src={wrench} alt="Desarrollo a medida" />
     ),
   },
 
