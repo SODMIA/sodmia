dc4d5a8408bc1952d80dd58d05361430fb6f7f47
SODMIA <167714181+SODMIA@users.noreply.github.com>
2024-08-27 19:13:34 -0500

    refactor: layout fit

--- app/(modules)/home/components/Hero.tsx
+++ app/(modules)/home/components/Hero.tsx
@@ -10,19 +10,20 @@ export function Hero() {
       <Image
         src={bg1}
         alt="Equipo de SODMIA trabajando en soluciones digitales"
- 
+        layout="fill"
         objectFit="cover"
         className="absolute inset-0 z-[-1]"
       />
       <div className="flex flex-col  justify-center items-start h-full max-w-screen-xl mx-auto px-4 py-16 sm:px-6 lg:px-8 lg:pt-32">
         <div className="text-white max-w-2xl">
           <h1 className="text-4xl pt-10 sm:text-5xl lg:text-6xl font-bold tracking-tight leading-none mb-4">
-            Impulsa tu negocio con la <span className="text-red-500">transformación digital</span>
+            Impulsa tu negocio con la{" "}
+            <span className="text-red-500">transformación digital</span>
           </h1>
           <p className="mb-6 text-lg sm:text-xl lg:text-2xl">
-            Desarrollamos soluciones tecnológicas a medida que optimizan tus procesos, 
-            potencian tus productos y revolucionan tus servicios. Con SODMIA, 
-            el éxito digital de tu organización está garantizado.
+            Desarrollamos soluciones tecnológicas a medida que optimizan tus
+            procesos, potencian tus productos y revolucionan tus servicios. Con
+            SODMIA, el éxito digital de tu organización está garantizado.
           </p>
           <Link href="#services">
             <Button size="sm" className="text-base sm:text-lg">
